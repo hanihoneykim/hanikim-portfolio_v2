6735fab6ad16cf2c38e85d2901f2093db44c66a4
hanihoneykim <107341819+hanihoneykim@users.noreply.github.com>
2023-10-05 20:19:30 +0900

    update

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -7,7 +7,7 @@ export default function About() {
         <>
             <Box w="100%" h="90vh" bgColor={"gray.300"}>
                 <HeaderAbout />
-                <Box w="100%" h="80vh" pt={14} px={14}>
+                <Box w="100%" h="70vh" pt={14} px={14}>
                     <Grid h="100%" gap={10} gridTemplateColumns={"1fr 1.5fr 1.5fr"} gridTemplateRows={"1fr 1fr"}>
 
                         <GridItem rowSpan={2}>
@@ -16,11 +16,11 @@ export default function About() {
 
                         <GridItem p={8} bg={"rgba(0,0,0,0.3)"} borderRadius={"20px"}>
                             <Flex>
-                                <Text fontSize={28} fontWeight={900}  mb={4} color="white">"비즈니스 성장을 통해 배우는 개발자"</Text>
+                                <Text fontSize={24} fontWeight={900}  mb={4} color="white">"비즈니스 성장을 통해 배우는 개발자"</Text>
                             </Flex>
-                            <Text fontSize={18} fontWeight={500} mb={3} color="white">회사의 성장과, 팀원들과의 협업을 최우선으로 합니다.</Text>
-                            <Text fontSize={18} fontWeight={500} mb={3} color="white">수년의 프리랜서 조명 디자이너 경험으로 몸담은 회사・팀의 성장이 곧 나의 성장이고, 발전이 곧 나의 공부임을 깨달았습니다.</Text>
-                            <Text fontSize={18} fontWeight={500} mb={1} color="white">회사에 속한 개발자로서 팀원들과 함께 협업하여 수치로 증명되는 웹 성능 개선에 기여하고 싶습니다.</Text>
+                            <Text fontSize={16} fontWeight={500} mb={3} color="white">회사의 성장과, 팀원들과의 협업을 최우선으로 합니다.</Text>
+                            <Text fontSize={16} fontWeight={500} mb={3} color="white">수년의 프리랜서 조명 디자이너 경험으로 몸담은 회사・팀의 성장이 곧 나의 성장이고, 발전이 곧 나의 공부임을 깨달았습니다.</Text>
+                            <Text fontSize={16} fontWeight={500} mb={1} color="white">회사에 속한 개발자로서 팀원들과 함께 협업하여 수치로 증명되는 웹 성능 개선에 기여하고 싶습니다.</Text>
                             
                         </GridItem>
 
@@ -39,7 +39,7 @@ export default function About() {
                         </GridItem>
 
                         <GridItem p={4} bg={"rgba(0,0,0,0.3)"} borderRadius={"20px"}>
-                            <Text fontSize={36} fontWeight={900}  m={3}>👩🏻‍💻 Who Am I?</Text>
+                            <Text fontSize={28} fontWeight={900}  m={3}>👩🏻‍💻 Who Am I?</Text>
                             <Grid  px={10} mt={7} h="80%" gridTemplateColumns={"1fr 2fr"}>
                                 <GridItem>
                                     <Text fontSize={18} fontWeight={700} mb={4}>이름 :</Text>
